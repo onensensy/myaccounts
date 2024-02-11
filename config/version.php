66cadf07ffbecf5414664f72454f9998bb0866a2
Onen Sensy <sensyonen@gmail.com>
2024-02-11 15:07:59 +0300

    Initial

---
+++ config/version.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'name'      =>  'Akaunting',
+
+    'code'      =>  'Speed',
+
+    'major'     =>  '3',
+
+    'minor'     =>  '1',
+
+    'patch'     =>  '5',
+
+    'build'     =>  '',
+
+    'status'    =>  'Stable',
+
+    'date'      =>  '20-December-2023',
+
+    'time'      =>  '13:00',
+
+    'zone'      =>  'GMT +3',
+
+];
